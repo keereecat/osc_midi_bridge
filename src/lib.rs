8ee64e82c793be21a3dfd853b7fc3308485e767b
Keeree Lewah <spots@keer.ee>
2018-07-25 21:19:58 -0700

    Fix lua error by changing signature of run()
    - Better error handling will be required in the future.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,9 @@ mod midi;
 extern crate rlua;
 
 use rlua::prelude::*;
-use std::error::Error;
 
 /// Main entry point to library.
-pub fn run() -> Result<(), Box<Error>> {
+pub fn run() -> Result<(), rlua::Error> {
     //FIXME: Is there a way to get this from cargo.toml?
     println!("OSC/MIDI Bridge ver. 0.1.0");
     println!("Initializing Lua... ");
